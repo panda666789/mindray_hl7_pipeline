1043a428a7696725d81fed7578c3f024be01e659
潘思琦 <13850676+panda123321@user.noreply.gitee.com>
2026-03-07 10:51:15 +0800

    all be done

--- apps/client/PhysRecorder.py
+++ apps/client/PhysRecorder.py
@@ -1285,10 +1285,8 @@ from tkinter import Menu
 try:
     import sv_ttk
     _HAS_SV_TTK = True
-    print("[INFO] sv-ttk loaded successfully")
 except ImportError:
     _HAS_SV_TTK = False
-    print("[WARN] sv-ttk not installed, using default theme")
 import cv2
 import os
 import time
@@ -1854,8 +1852,7 @@ class SensorApp(tk.Tk):
         self.geometry("1100x1100")
 
         if _HAS_SV_TTK:
-            sv_ttk.set_theme("dark")
-            print("[INFO] sv-ttk dark theme applied")
+            sv_ttk.set_theme("light")
         self.name_cam1 = f'{time.time()}'
         self.name_cam2 = f'{time.time()}'
         
